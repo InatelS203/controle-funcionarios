75cbd3ec49624954be6e29de2a7b111f043abb63
Bianca Ribeiro de Souza <biancaridesouza@gmail.com>
2024-11-19 16:24:32 -0300

    Add files via upload

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from "../pages/Login";
+import { NewEmployee } from "../pages/NewEmployee";
 
 export const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/cadastrar-funcionario" element={<NewEmployee />} />
       </Routes>
     </BrowserRouter>
   );
